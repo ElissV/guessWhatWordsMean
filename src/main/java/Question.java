417642464aa11035579091e590d12b7042816b39
8f4md201gv <elis.vassiltsova.v@gmail.com>
2020-02-21 23:55:39 +0200

    Question class added

---
+++ src/main/java/Question.java
@@ -0,0 +1,12 @@
+public class Question {
+
+    private String chosenWord;
+    private String chosenWordDefeinition;
+    private String[] otherOptionsForAnswer;
+    private int otherOptionsQTY = 3;
+
+    Question() {
+        
+    }
+
+}
