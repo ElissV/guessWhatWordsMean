482650d08ebd3fdf0bed2fcd277e809cdffc3a02
8f4md201gv <elis.vassiltsova.v@gmail.com>
2020-02-22 22:02:05 +0200

    Game class deleted. All fields and methods moved to Main class

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -3,10 +3,18 @@ import javax.swing.*;
 public class MainWindow {
 
     private static GameForm gameForm;
+    private Question question;
+    private static final int QUESTIONS_QTY = 10;
+    private int questionsAnswered = 0;
+    private int rightAnswersGiven = 0;
 
     public static void main(String[] args) {
         gameForm = new GameForm();
-        new Game().askQuestion();
+        askQuestion();
+    }
+
+    void askQuestion() {
+        question = new Question();
     }
 
     static JFrame getGameFormFrame() {
